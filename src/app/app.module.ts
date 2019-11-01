56b495389241ebd38e827ab03fb39a1a78aa5cfc
bext <jalbertomr@yahoo.com>
2019-10-31 18:44:34 -0600

    primer commit app, miComponente generado sin integrar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponenteComponent
   ],
   imports: [
     BrowserModule
