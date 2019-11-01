b1aec3327f6a24f9363a25ce3a85f0b5b85dad07
bext <jalbertomr@yahoo.com>
2019-10-31 19:03:59 -0600

    Xcomponente.ts templateUrl->template con `... permite varios inlines ...`

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-mi-componente]',
-  template: '<div><h1>inline template</h1></div>',
+  template: `<div><h1>Con \`...\` permite varias lineas inline<br>
+                inline template ...linea 1<br>
+                inline template ...linea 2<br>
+                inline template ...linea 3<br>
+            </h1></div>`,
   styleUrls: ['./mi-componente.component.css']
 })
 export class MiComponenteComponent implements OnInit {
