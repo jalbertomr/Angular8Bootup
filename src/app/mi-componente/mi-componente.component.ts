fe9814b5ae881b48b6de7278280ea9e19e72661b
bext <jalbertomr@yahoo.com>
2019-10-31 20:35:31 -0600

    ejemplos de interpolation

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: '[app-mi-componente]',
-  template: `<div><h1>Con \`...\` permite varias lineas inline<br>
-                inline template ...linea 1<br>
-                inline template ...linea 2<br>
-                inline template ...linea 3<br>
-      styleUrls -> styles [\`div (se aplica css) \`]
+  template: `<div><h1> Interpolation<br>
+             - Valor tomado de atributo de clase = {{miValor}}<br>
+             - {{12.23 + 34}}<br>
+             - {{"Concatenando strings " + miValor}}<br>
+             - longitud de miValor: {{miValor.length}}<br>
+            - "miValor a UpperCase:  + {{miValor.toLocaleUpperCase()}}<br>
+            - llamando metodoA: {{metodoA()}}<br>
+            - Asignacion no es permitida: a = 2 + 2 <br>
+            - global js variables no son observadas: window.location.href: <br>
+            - pero si por variable de clase: {{siteUrl}}<br>
+            - ejemplo: public siteUrl = window.location.href;
             </h1></div>`,
-  styles: [`
-  div {
-    color: orange;
-    }
-  `]
+  styles: []
 })
 export class MiComponenteComponent implements OnInit {
+
+  public miValor = "valorXX";
+  public siteUrl = window.location.href;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  metodoA() {
+    return "valor regresado x metodoA.";
+  }
 }
