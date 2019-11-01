af71f1e7e11e25dd29f3d0a25c2b2b7b9ac02e1d
bext <jalbertomr@yahoo.com>
2019-10-31 18:59:49 -0600

    Xcomponente.ts templateUrl->template inline

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-mi-componente',
-  templateUrl: './mi-componente.component.html',
+  selector: '[app-mi-componente]',
+  template: '<div><h1>inline template</h1></div>',
   styleUrls: ['./mi-componente.component.css']
 })
 export class MiComponenteComponent implements OnInit {
