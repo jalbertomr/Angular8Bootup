45e619d46f76cc07f699d2847637e3d93e3d7e2b
bext <jalbertomr@yahoo.com>
2019-11-01 21:22:05 -0600

    Property Binding Attribute - Property. (2)

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -3,19 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-mi-componente]',
   template: `<div><h1> Property binding</h1><br>
-      <table><tr><th>Attribute</th><th>Property</th></tr>
-          <tr><td>HTML.</td><td>DOM (Document Object Model).</td></tr>
-          <tr><td>Initialized.</td><td>Initialized by Attribute.</td></tr>
-          <tr><td>Not Change.</td><td>Can Change.</td></tr>
-      </table>
-             <input type="text" value="Bext">
+             <input [id]="miClassProperty" type="text" value="Bext">
+             <input bind-disabled="isDisabled" id={{miClassProperty}} type="text" value="Bext">
             </div>`,
   styles: []
 })
 export class MiComponenteComponent implements OnInit {
 
   public miValor = "valorXX";
-  public siteUrl = window.location.href;
+  public miClassProperty = 12345;
+  public isDisabled = true;
 
   constructor() { }
 
