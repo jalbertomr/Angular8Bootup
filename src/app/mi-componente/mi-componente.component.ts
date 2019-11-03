7761d66d6b992ed4499e4c4dbce369e32e9f32de
bext <jalbertomr@yahoo.com>
2019-11-02 20:35:07 -0600

    event Binding

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -2,23 +2,17 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({selector: '[app-mi-componente]',
   template: `
-      <div><h1> style Binding</h1><br>
-          <h2 [style.color]="'orange'">Aplicando [style.color]="'orange'"</h2>
-          <h3>miBooleanProperty = {{miBooleanProperty}}</h3>
-          <h2 [style.color]="miBooleanProperty ? 'green' : 'red'">Aplicando [style.color]="miBooleanProperty ? 'green' :
-              'red'"</h2>
-          <h3>miColorProperty = {{miColorProperty}}</h3>
-          <h2 [style.color]="miColorProperty">Aplicando [style.color]="miColorProperty"</h2>
-          <h3>miPropertyVariosStyles = {{miPropertyVariosStyles.color}},{{miPropertyVariosStyles.fontStyle}}</h3>
-          <h2 [ngStyle]="miPropertyVariosStyles">Aplicando [ngStyle]="miPropertyVariosStyles"</h2>
-          <button (click)="Cambia()"><h2>Cambia property values -> styles</h2></button>
+      <div><h1> event Binding</h1><br>
+          <h2>miPropertyNombre = {{miPropertyNombre}}  </h2>
+          <h2>miPropertyTexto = {{miPropertyTexto}} </h2>
+          <button (click)="onClick($event)"><h2>button (click)="onClick()"</h2></button>
+          <button (click)="miPropertyTexto='asignado directo en template (vista).'"><h2>button (click)="miPropertyTexto='asignado directo en template (vista).'"</h2></button>
       </div>`,
   styles: []
 })
 export class MiComponenteComponent implements OnInit {
-  public miColorProperty = "blue";
-  public miBooleanProperty = true;
-  public miPropertyVariosStyles = {color: this.miColorProperty, fontStyle: "italic"};
+  public miPropertyNombre = "Beto";
+  public miPropertyTexto = "";
 
   constructor() {
   }
@@ -26,17 +20,8 @@ export class MiComponenteComponent implements OnInit {
   ngOnInit() {
   }
 
-  metodoA() {
-    return "valor regresado x metodoA.";
-  }
-
-  Cambia() {
-    this.miBooleanProperty = !this.miBooleanProperty;
-    if (this.miColorProperty == 'blue') this.miColorProperty = 'orange'; else this.miColorProperty = 'blue';
-    (this.miPropertyVariosStyles.fontStyle == 'italic') ? this.miPropertyVariosStyles.fontStyle = 'normal' : this.miPropertyVariosStyles.fontStyle = 'italic';
-  }
-
-  metodoA() {
-    return "valor regresado x metodoA.";
+  onClick(event) {
+    this.miPropertyTexto=event.type;
+    console.log(event);
   }
 }
