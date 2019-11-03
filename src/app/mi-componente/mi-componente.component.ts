eae2a6fd99fa30fc4e20b5d7a2d642d5ce33453b
bext <jalbertomr@yahoo.com>
2019-11-02 19:29:55 -0600

    class Binding

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -2,19 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-mi-componente]',
-  template: `<div><h1> Property binding</h1><br>
-             <input [id]="miClassProperty" type="text" value="Bext">
-             <input bind-disabled="isDisabled" id={{miClassProperty}} type="text" value="Bext">
-            </div>`,
-  styles: []
+  template: `
+      <div><h1> class Binding</h1><br>
+          <h2 class="text-success">Aplicando class class="text-success"</h2>
+          <h2 [class]="miClassProperty">Aplicando class binding [class]="miClassProperty"</h2>
+          <h2 class="text-danger" [class]="miClassProperty">Aplicando class y [class], solo [class] aplica</h2>
+          <h2 [class.text-danger]="miBooleanProperty">Aplicando [class.text-danger]="miBooleanProperty"</h2>
+          <h2 [ngClass]="miPropertyVariasClasses">Aplicando [ngClass]="miPropertyVariasClasses"</h2></div>`,
+  styles: [`    .text-success {
+      color: green;
+  }
+
+  .text-danger {
+      color: red;
+  }
+
+  .text-special {
+      font-style: italic;
+  }  `]
 })
 export class MiComponenteComponent implements OnInit {
+  public miClassProperty = "text-special";
+  public miBooleanProperty = true;
+  public hasError = true;
+  public isSpecial = true;
+  public miPropertyVariasClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
 
-  public miValor = "valorXX";
-  public miClassProperty = 12345;
-  public isDisabled = true;
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
