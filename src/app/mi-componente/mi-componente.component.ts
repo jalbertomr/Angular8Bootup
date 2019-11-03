abf63ce2331a05bcb42b9424045758937c90d797
bext <jalbertomr@yahoo.com>
2019-11-02 21:27:59 -0600

    Template Reference Binding

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -2,17 +2,16 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({selector: '[app-mi-componente]',
   template: `
-      <div><h1> event Binding</h1><br>
-          <h2>miPropertyNombre = {{miPropertyNombre}}  </h2>
-          <h2>miPropertyTexto = {{miPropertyTexto}} </h2>
-          <button (click)="onClick($event)"><h2>button (click)="onClick()"</h2></button>
-          <button (click)="miPropertyTexto='asignado directo en template (vista).'"><h2>button (click)="miPropertyTexto='asignado directo en template (vista).'"</h2></button>
+      <div><h1> Template Reference Variable</h1><br>
+          <textarea disabled><input #miInput type="text"></textarea><br>
+          <input #miInput type="text"><br>
+          <button (click)="logMessage(miInput)"><h3>button (click)="logMessage(miInput)"</h3></button>
+          <h2>Valor Capturado = "{{miValorCapturado}}"</h2>
       </div>`,
   styles: []
 })
 export class MiComponenteComponent implements OnInit {
-  public miPropertyNombre = "Beto";
-  public miPropertyTexto = "";
+  public miValorCapturado = "";
 
   constructor() {
   }
@@ -20,8 +19,8 @@ export class MiComponenteComponent implements OnInit {
   ngOnInit() {
   }
 
-  onClick(event) {
-    this.miPropertyTexto=event.type;
-    console.log(event);
+  logMessage(miInput: HTMLInputElement) {
+    console.log(miInput);
+    this.miValorCapturado = miInput.value;
   }
 }
