94fa357d5742295a7f1dba2909891cf7fc911a94
bext <jalbertomr@yahoo.com>
2019-11-02 21:42:45 -0600

    Two Way Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './mi-componente/mi-componente.component';
@@ -10,7 +11,8 @@ import { MiComponenteComponent } from './mi-componente/mi-componente.component';
     MiComponenteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -2,11 +2,10 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({selector: '[app-mi-componente]',
   template: `
-      <div><h1> Template Reference Variable</h1><br>
-          <textarea disabled><input #miInput type="text"></textarea><br>
-          <input #miInput type="text"><br>
-          <button (click)="logMessage(miInput)"><h3>button (click)="logMessage(miInput)"</h3></button>
-          <h2>Valor Capturado = "{{miValorCapturado}}"</h2>
+      <div><h1> Two Way Binding</h1><br>
+          <textarea disabled><input  [(ngModel)]="miValorCapturado" type="text"></textarea><br>
+          <input  [(ngModel)]="miValorCapturado" type="text"><br>
+          <h2>property miValorCapturado = "{{miValorCapturado}}"</h2>
       </div>`,
   styles: []
 })
@@ -19,8 +18,4 @@ export class MiComponenteComponent implements OnInit {
   ngOnInit() {
   }
 
-  logMessage(miInput: HTMLInputElement) {
-    console.log(miInput);
-    this.miValorCapturado = miInput.value;
-  }
 }
