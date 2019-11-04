ab55cc5854adcbe15b9ea8f68f75bd7a7abe7f53
bext <jalbertomr@yahoo.com>
2019-11-03 18:09:29 -0600

    *ngIf Directive

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -2,17 +2,27 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({selector: '[app-mi-componente]',
   template: `
-      <div><h1> Two Way Binding</h1><br>
-          <textarea disabled><input  [(ngModel)]="miValorCapturado" type="text"></textarea><br>
-          <input  [(ngModel)]="miValorCapturado" type="text"><br>
-          <h2>property miValorCapturado = "{{miValorCapturado}}"</h2>
+      <div><h1> *ngIf Directive</h1><br>
+          <textarea disabled><h2 *ngIf="miPropertyDisplayText">miPropertyDisplayText = "{{miPropertyDisplayText}}"</h2></textarea><br>
+          
+          <h2 *ngIf="miPropertyDisplayText">miPropertyDisplayText = "{{miPropertyDisplayText}}"</h2>
+          <button (click)="cambio()">Alternar</button>
+
       </div>`,
   styles: []
 })
+
 export class MiComponenteComponent implements OnInit {
-  public miValorCapturado = "";
+  public miPropertyValue = "algo";
+  public miPropertyDisplayText = true;
 
   constructor() {
+  }
+
+  cambio() {
+    console.log("IN cambio()");
+    this.miPropertyDisplayText == true ? this.miPropertyDisplayText = false : this.miPropertyDisplayText = true;
+    console.log("this.miPropertyDisplayText: " + this.miPropertyDisplayText);
   }
 
   ngOnInit() {
