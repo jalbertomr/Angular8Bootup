0292600c499a8529d392c015d18748b3d83e7568
bext <jalbertomr@yahoo.com>
2019-11-04 01:31:08 -0600

    *ngFor="let color of colors" ... <h2>{{color}}</h2>

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -2,41 +2,23 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({selector: '[app-mi-componente]',
   template: `
-      <div><h1> *ngSwitch Directive</h1><br>
-          <textarea disabled><div [ngSwitch]="miPropertyCaseOptions">
-              <div *ngSwitchCase="'rojo'"><h3>ALERTA</h3></div>
-              <div *ngSwitchCase="'azul'"><h3>CIELO</h3></div>
-              <div *ngSwitchCase="'verde'"><h3>ARBOL</h3></div>
-              <div *ngSwitchDefault><h3>VALOR NO IDENTIFICADO</h3></div>
+      <div><h1> *ngFor Directive</h1><br>
+          <textarea disabled><div *ngFor="let color of arrColors">
+             <h2>{{color}}{{soloParaMostrar}}</h2>
           </div>
           </textarea><br>
-          <h3>miPropertyCaseOptions  = {{miPropertyCaseOptions}}</h3>
-          <div [ngSwitch]="miPropertyCaseOptions">
-              <div *ngSwitchCase="'rojo'"><h3>ALERTA</h3></div>
-              <div *ngSwitchCase="'azul'"><h3>CIELO</h3></div>
-              <div *ngSwitchCase="'verde'"><h3>ARBOL</h3></div>
-              <div *ngSwitchDefault><h3>VALOR NO IDENTIFICADO</h3></div>
+          <div *ngFor="let color of arrColors">
+             <h2>{{color}}</h2>
           </div>
-          <button (click)="cambio()">Alternar</button>
       </div>`,
   styles: []
 })
 
 export class MiComponenteComponent implements OnInit {
-  public miPropertyCaseOptions = "rojo";
+  public arrColors = ['rojo','azul','verde','amarillo','blanco'];
+  public soloParaMostrar = "{{color}}";
 
   constructor() {
-  }
-
-  cambio() {
-    console.log("IN cambio()");
-    var arrColors = ['rojo','azul','verde','amarillo','blanco'];
-    console.log(Math.trunc((Math.random()*arrColors.length)));
-
-    this.miPropertyCaseOptions = arrColors[Math.trunc((Math.random()*arrColors.length))];
-    //  this.miPropertyCaseOptions = false :
-    //  this.miPropertyCaseOptions = true;
-    console.log("this.miPropertyCaseOptions: " + this.miPropertyCaseOptions);
   }
 
   ngOnInit() {
